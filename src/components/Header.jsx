4b43fff39221f6cd04a929dcb4bd2af8d66a13a5
John Wang <john-w@berkeley.edu>
2025-01-11 23:46:49 -0800

    Header in place, components re-orgd, assets loaded

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import NotionFace from "../assets/notion-face.png"
+
+export default function Header() {
+    return (
+        <header>
+            <img src={NotionFace}/>
+            <h1>John Wang</h1>
+        </header>
+    )
+}
